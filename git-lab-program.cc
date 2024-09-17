f5a014725e414baaf4075e191f3fa72454194031
Toby Cremer <tc850522@ohio.edu>
2024-09-17 13:12:01 -0400

    code

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Toby Cremer
+ *        Date: 09/17/2024
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
